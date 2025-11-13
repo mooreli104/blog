4f0f64d8a530a47c252b3da58910131d9df33e79
mooreli104 <moore_li4@outlook.com>
2025-11-12 20:50:50 -0500

    Update v2

--- src/config.ts
+++ src/config.ts
@@ -61,18 +61,18 @@ export const WORK_EXPERIENCE = [
 
 
 export const PROJECTS = [
-    // {
-    //   id: "my-first-rocket-launch",
-    //   title: "My First Rocket Launch",
-    //   description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
-    //   image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    //   externalLink: "https://www.nasa.gov/",
-    //   details: [
-    //     "Explaining space exploration concepts through creative illustrations.",
-    //     "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
-    //     "Won an award in the interstellar creativity competition.",
-    //   ],
-    // },
+    {
+      id: "hackohio",
+      title: "Hack/Ohio 2025",
+      description: "My first hackathon project where I built a real-time dashboard and analytics platform for monitoring soil moisture, temperature, humidity, etc.",
+      image: '/farming.jpg',
+      externalLink: "",
+      details: [
+        "Explaining space exploration concepts through creative illustrations.",
+        "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
+        "Won an award in the interstellar creativity competition.",
+      ],
+    },
   ];
   
 export const GOOGLE_GTAG = "";
